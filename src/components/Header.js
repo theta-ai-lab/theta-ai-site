f7a80c7841c46ffeaf5b14a335708649d31b54c1
djib2011 <thanostagaris@gmail.com>
2025-02-16 19:40:00 +0200

    fear: Disable mode toggle button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,39 +4,40 @@ import './Header.css';
 
 function Header({ darkMode, toggleMode }) {
   return (
-    <header className={`navbar ${darkMode ? 'dark-mode' : 'light-mode'}`}>
-      <div className="logo">
-        {/* Use Unicode character U+03D1 (θ) */}
-        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+      <header className={`navbar ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+        <div className="logo">
+          {/* Use Unicode character U+03D1 (θ) */}
+          <Link to="/" style={{textDecoration: 'none', color: 'inherit'}}>
           <span className="theta-logo">
             &#x03D1; {/* Unicode for theta */}
           </span>
-        </Link>
-      </div>
-      <nav>
-        <ul className="nav-list">
-          <li>
-            <Link to="/" className="nav-link">
-              Company
-            </Link>
-          </li>
-          <li>
-            <Link to="/team" className="nav-link">
-              Team
-            </Link>
-          </li>
-          <li>
-            <Link to="/projects" className="nav-link">
-              Projects
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <button onClick={toggleMode} className="mode-toggle">
-        {darkMode ? '🌙' : '☀️'}
-      </button>
-    </header>
-  );
+          </Link>
+        </div>
+        <nav>
+          <ul className="nav-list">
+            <li>
+              <Link to="/" className="nav-link">
+                Company
+              </Link>
+            </li>
+            <li>
+              <Link to="/team" className="nav-link">
+                Team
+              </Link>
+            </li>
+            <li>
+              <Link to="/projects" className="nav-link">
+                Projects
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/*<button onClick={toggleMode} className="mode-toggle">*/}
+          <button className="mode-toggle">
+            {darkMode ? '🌙' : '☀️'}
+          </button>
+      </header>
+);
 }
 
 export default Header;
