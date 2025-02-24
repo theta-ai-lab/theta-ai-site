230b610752ff24f653005ab6fd2470c2d2832adf
djib2011 <thanostagaris@gmail.com>
2025-02-24 18:12:04 +0200

    feat: Remove sitemap from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,17 +8,17 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-section sitemap">
-          <h4>Site Map</h4>
-          <ul className="horizontal-nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/company">Company</Link></li>
-            <li><Link to="/team">Team</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-          </ul>
-        </div>
-        <div className="footer-section nothing">
-        </div>
+        {/*<div className="footer-section sitemap">*/}
+        {/*  <h4>Site Map</h4>*/}
+        {/*  <ul className="horizontal-nav">*/}
+        {/*    <li><Link to="/">Home</Link></li>*/}
+        {/*    <li><Link to="/company">Company</Link></li>*/}
+        {/*    <li><Link to="/team">Team</Link></li>*/}
+        {/*    <li><Link to="/projects">Projects</Link></li>*/}
+        {/*  </ul>*/}
+        {/*</div>*/}
+        {/*<div className="footer-section nothing">*/}
+        {/*</div>*/}
         <div className="footer-section company-info">
         <h4>Company Info</h4>
           <ul className="">
@@ -50,7 +50,7 @@ const Footer = () => {
               <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 width="24" 
-                height="24" 
+                height="24"
                 fill="currentColor" 
                 viewBox="0 0 24 24"
               >
