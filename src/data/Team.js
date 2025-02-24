7c98313e6cbbd1d29508b642b004bb6bbbeeef59
djib2011 <thanostagaris@gmail.com>
2025-02-24 17:36:32 +0200

    feat: Add new team member

--- src/data/Team.js
+++ src/data/Team.js
@@ -1,6 +1,7 @@
 import pantosPhoto from '../data/team_photos/pantos_photo_3.png';
 import tagarisPhoto from '../data/team_photos/tagaris_photo_3.jpg';
 import tasakosPhoto from '../data/team_photos/tasakos_photo.jpg';
+import tasarasPhoto from '../data/team_photos/tasaras_photo.jpg';
 
 export const teamData = [
   {
@@ -18,21 +19,32 @@ He also serves as an instructor at Code.Hub, having trained more than 60 profess
     name: "Thanos Tagaris",
     title: "Senior Machine Learning Engineer",
     education: "PhD in Deep Learning \n(NTUA, 2020)",
-    photo: tagarisPhoto, // Update with your actual image path
+    photo: tagarisPhoto,
     bio: `Thanos has over 5 years of professional experience working for various firms, most recently Netcompany/EUIPO and Deeplab/Taboola.
 Additionally, he has multiple years of research experience during his PhD and post-doc in various research areas (neural network interpretability, computer vision, timeseries forecasting, etc.).
 Finally, he has 7 years of teaching experience as an ML instructor at Code.Hub and a Lecturer at ATH/TECH college.`,
-    linkedin: "https://www.linkedin.com/in/thanos-tagaris-91b1401a1/", // Add LinkedIn URL if available
+    linkedin: "https://www.linkedin.com/in/thanos-tagaris-91b1401a1/",
   },
   {
     id: 3,
     name: "Thanos Tasakos",
     title: "Senior Machine Learning Engineer",
-    education: "Phd in Deep Reinforcement Learning \n(NTUA-present)",
+    education: "PhD in Deep Reinforcement Learning \n(NTUA, present)",
     photo: tasakosPhoto,
     bio: `With over 5 years of professional experience, Thanos has worked most notably as a Senior Machine Learning Engineer for Netcompany/EUIPO.
 He has also contributed to various EU research projects, including H2020.
 Additionally, Thanos has over 4 years of teaching experience as a Machine Learning Instructor at Code.Hub.`,
-    linkedin: "https://www.linkedin.com/in/thanos-tasakos-09382613a/", // Add LinkedIn URL if available
+    linkedin: "https://www.linkedin.com/in/thanos-tasakos-09382613a/",
   },
+  {
+    id: 3,
+    name: "Tasos Papagiannis",
+    title: "Machine Learning Engineer",
+    education: "PhD in Artificial Intelligence & Machine Learning (NTUA, 2024)",
+    photo: tasarasPhoto,
+    bio: `Tasos has lately worked as a Machine Learning Engineer for Extrabit, applying advanced ML techniques for time series forecasting. 
+He also collaborated on research projects for over 3 years during his PhD, focusing on tree search optimization, generative models 
+and reinforcement learning among others.Beyond these, he has 4 years of experience instructing at data science bootcamps at Code.Hub.`,
+    linkedin: "https://www.linkedin.com/in/tasos-papagiannis-541106315/",
+  }
 ];
