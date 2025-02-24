5fcd5139b4fcb6335729738ec965378202366cd8
djib2011 <thanostagaris@gmail.com>
2025-02-25 00:16:11 +0200

    refactor: Move projects into new patrners page

---
+++ src/data/Partners.js
@@ -0,0 +1,43 @@
+export const partnersData = [
+  {
+    id: 1,
+    name: "EUIPO", // TODO: Fix bug where alignment breaks with long names (European Union Intellectual Property Office)
+    thumbnail: "https://european-union.europa.eu/sites/default/files/styles/oe_theme_medium_no_crop/public/2021-02/EUIPO.jpg?itok=qAM2qMUv",
+    shortDescription: "AI tool development and support",
+    details:
+      "Theta AI's team members have played a pivotal role in various projects for the European Union Intellectual Property Office (EUIPO) as part of a Netcompany project. They have developed and supported many AI tools tailored to meet EUIPO's needs, enhancing the management and protection of intellectual property across Europe. Leveraging their expertise in machine learning and data science, the team has designed and implemented advanced algorithms to streamline processes and improve data analysis capabilities. This partnership underscores Theta AI's commitment to supporting EUIPO's mission of fostering innovation and creativity within the European Union."
+  },
+  {
+    id: 2,
+    name: "Netcompany",
+    thumbnail: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc.smartrecruiters.com%2Fsr-company-images-prod-aws-dc5%2F5c177432713c090007fe76a3%2Fdefault_social_logo%2F300x300%3Fr%3Ds3-eu-central-1%26_1694433513347&f=1&nofb=1&ipt=672d54944564893c6473aef167fcad32975630e9995a88e455e7d7229a9cce42&ipo=images",
+    shortDescription: "AI solution development",
+    details:
+      "Theta AI has been a major contributor to Netcompany's contract with EUIPO, providing expertise in Machine Learning. Additionally, it has supported the implementation of various AI solutions for private sector clients, such as NBG and HEDNO."
+  },
+  {
+    id: 3,
+    name: "Janea Systems",
+    thumbnail: "https://media.glassdoor.com/sql/7496248/janea-systems-squarelogo-1658391245327.png",
+    shortDescription: "ML solution development",
+    details:
+      "Theta AI has an upcoming partnership with Janea Systems for the development of AI and ML solutions for clients of Janea, including Microsoft and FAIR."
+  },
+  {
+    id: 4,
+    name: "Code.Hub",
+    thumbnail: "https://media.glassdoor.com/sql/4345640/code-hub-squarelogo-1627541068466.png",
+    shortDescription: "Delivering technical training programs for employees and university graduates",
+    details:
+      "Theta AI has delivered technical training to over 500 participants in more than 20 programs organized by Code.Hub. These mainly involve corporate training (Pfizer, NBG, Alpha Bank) and upskilling academies (with partners like TITAN, Microsoft, Piraeus Bank and Papastratos). The subject areas revolve around Python, Data Science, Machine Learning, Artificial intelligence and ML Ops."
+  },
+  {
+    id: 5,
+    name: "Athens Tech College",
+    // thumbnail: "https://womenleaders.gr/wp-content/uploads/2013/05/athtech-Logo-s.png",
+    thumbnail: "https://www.advertising.gr/uploads/images/103334/athens-tech-college-seminario-gia-to-synchrono-management-medet.jpg?lm=a6e302374ae50788157141a9d9166a1f",
+    shortDescription: "Teaching MSc academic courses in AI/ML topics",
+    details:
+      "Theta AI members are heavily involved in Athens Tech College's MSc in Big Data Engineering & Data Science, where they teach the 'Advanced Artificial Intelligence', 'Deep Learning', 'Data Mining and Machine Learning', 'Big Data', 'DevOps Engineering' and 'Continuous & Agile Software Engineering' courses. Formerly, they have also taught in various other under and post-graduate courses such as 'Artificial Intelligence' and 'Fundamentals of Computer Science' at the MSc in Software Development, as well as 'Artificial Intelligence Techniques', 'Programming Principles and Applications', 'Statistics' and 'Probabilities' in the BSc in Computer Science."
+  }
+];
--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,36 +1,10 @@
 export const projectsData = [
   {
     id: 1,
-    name: "EUIPO",
-    thumbnail: "https://european-union.europa.eu/sites/default/files/styles/oe_theme_medium_no_crop/public/2021-02/EUIPO.jpg?itok=qAM2qMUv",
-    shortDescription: "Develop AI tools for the European Union Intellectual Property Office",
-    details:
-      "Theta AI's team members have played a pivotal role in various projects for the European Union Intellectual Property Office (EUIPO). They have developed and supported many AI tools tailored to meet EUIPO's needs, enhancing the management and protection of intellectual property across Europe. Leveraging their expertise in machine learning and data science, the team has designed and implemented advanced algorithms to streamline processes and improve data analysis capabilities. This partnership underscores Theta AI's commitment to supporting EUIPO's mission of fostering innovation and creativity within the European Union."
-  },
-  {
-    id: 2,
     name: "Anomaly Detection",
     thumbnail: "https://cdn.techinasia.com/data/images/zT40dSboSFeT0TIzEDarRPBRzRl1aaesTYzW9nvP.jpeg",
     shortDescription: "Develop an anomaly detection system for industrial manufacturer",
     details:
-      "Theta AI has developed an advanced anomaly detection system for a leading electrical protection and connectivity solutions manufacturer. This system leverages machine learning algorithms to monitor critical infrastructure in real time. By identifying unusual patterns and potential issues, it helps clients proactively address problems before they escalate, enhancing operational efficiency and minimizing downtime."
+      "Theta AI has an upcoming contract to develop an advanced anomaly detection system for a leading electrical protection and connectivity solutions manufacturer. This system will leverage machine learning algorithms to monitor critical infrastructure in real time. By identifying unusual patterns and potential issues, it will help the client proactively address problems before they escalate, enhancing operational efficiency and minimizing downtime."
   },
-  {
-    id: 3,
-    name: "Professional Training",
-    thumbnail: "https://media.glassdoor.com/sql/4345640/code-hub-squarelogo-1627541068466.png",
-    shortDescription: "Delivering technical training programs for employees and university graduates",
-    details:
-      "Theta AI has delivered technical training to over 500 participants in more than 20 programs organized by Code.Hub. These mainly involve corporate training (Pfizer, NBG, Alpha Bank) and upskilling academies (with partners like TITAN, Microsoft, Piraeus Bank and Papastratos). The subject areas revolve around Python, Data Science, Machine Learning, Artificial intelligence and ML Ops."
-  },
-  {
-    id: 4,
-    name: "Academic Teaching",
-    // thumbnail: "https://womenleaders.gr/wp-content/uploads/2013/05/athtech-Logo-s.png",
-    thumbnail: "https://www.advertising.gr/uploads/images/103334/athens-tech-college-seminario-gia-to-synchrono-management-medet.jpg?lm=a6e302374ae50788157141a9d9166a1f",
-    shortDescription: "Teaching MSc academic courses in AI/ML topics",
-    details:
-      "Theta AI members are heavily involved in Athens Tech College's MSc in Big Data Engineering & Data Science, where they teach the 'Advanced Artificial Intelligence', 'Deep Learning', 'Data Mining and Machine Learning', 'Big Data', 'DevOps Engineering' and 'Continuous & Agile Software Engineering' courses. Formerly, they have also taught in various other under and post-graduate courses such as 'Artificial Intelligence' and 'Fundamentals of Computer Science' at the MSc in Software Development, as well as 'Artificial Intelligence Techniques', 'Programming Principles and Applications', 'Statistics' and 'Probabilities' in the BSc in Computer Science."
-  }
-
 ];
